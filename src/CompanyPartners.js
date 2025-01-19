b047a501e2703fe5d7c14117f12469ecf0a9f4a3
PSURI1894 <parthsuri009@gmail.com>
2025-01-20 02:23:12 +0530

    Responsive only left

--- src/CompanyPartners.js
+++ src/CompanyPartners.js
@@ -3,15 +3,19 @@ import "./CompanyPartners.css";
 
 const CompanyPartners = ({ partners }) => {
   return (
-    <div className="partners-container">
-      <h3>Company Partners :</h3>
-      <div className="partners-logos">
-        {partners.map((partner, index) => (
-          <div key={index} className="partner-card">
-            <img src={partner.logo} alt={`${partner.name} logo`} />
-            <p>{partner.name}</p>
-          </div>
-        ))}
+    <div>
+      <div className="partners-containerabove">
+        <h2>Company Partners :</h2>
+      </div>
+      <div className="partners-container">
+        <div className="partners-logos">
+          {partners.map((partner, index) => (
+            <div key={index} className="partner-card">
+              <img src={partner.logo} alt={`${partner.name} logo`} />
+              <p>{partner.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
